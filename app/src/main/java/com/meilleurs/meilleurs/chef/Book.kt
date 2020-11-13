cddf693fdc9c750d885354c9e88a4446fee6a72c
Ismail EL KHANTACHE <ismail.elkhantache@amplexor.com>
2020-11-13 16:17:46 +0100

    Add new library fragment

---
+++ app/src/main/java/com/meilleurs/meilleurs/chef/Book.kt
@@ -0,0 +1,3 @@
+package com.meilleurs.meilleurs.chef
+
+data class Book(val isbn: String, val title: String, val price: String, val cover: String)
---
+++ app/src/main/java/com/meilleurs/meilleurs/chef/fragments/library/MyBookRecyclerViewAdapter.kt
@@ -0,0 +1,42 @@
+package com.meilleurs.meilleurs.chef.fragments.library
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+import com.meilleurs.meilleurs.chef.Book
+import com.meilleurs.meilleurs.chef.R
+
+class MyBookRecyclerViewAdapter(
+    private val values: List<Book>
+) : RecyclerView.Adapter<MyBookRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_library, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.title
+        holder.contentView.text = item.isbn
+        holder.idView.setOnClickListener { v ->
+            Snackbar.make(v, item.title + " added to your cart !", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val idView: TextView = view.findViewById(R.id.item_number)
+        val contentView: TextView = view.findViewById(R.id.content)
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+}
